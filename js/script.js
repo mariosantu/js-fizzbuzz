9a76459fc3b8683ad62ca0c4094e3801a8d6d13d
Mario Santucci <mario.santucci95@gmail.com>
2021-03-23 14:08:27 +0100

    creazione repo e file + creazione array numeri

---
+++ js/script.js
@@ -0,0 +1,14 @@
+//creazione arrey numeri da 1 a 100
+
+var arrayNumeri = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21]
+
+//Stampo i numeri
+
+for( var i = 0; i < arrayNumeri.length; i++) {
+    var numeriDaUnoACento = arrayNumeri[i]; 
+    console.log(numeriDaUnoACento);
+
+    
+    
+   
+}
