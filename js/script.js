1b69a4ce0f9973d5588d39cb69b5c2e73962f569
Mario Santucci <mario.santucci95@gmail.com>
2021-03-23 16:04:57 +0100

    correzione posizione sbagliata di &&

--- js/script.js
+++ js/script.js
@@ -1,15 +1,17 @@
 //Creo range da 1 a 100 e stampo i numeri
 
-for( var i = 1; i < 101; i++) {
+for( var i = 1; i <= 100; i++) {
     var numeriDaUnoACento = i ; 
-    console.log(numeriDaUnoACento);
 
-    if (numeriDaUnoACento % 3 === 0 ) {
-        console.log('Fizz');
+    if (numeriDaUnoACento % 3 === 0 && numeriDaUnoACento % 5 === 0) {
+        var FizzBuzzMsg = numeriDaUnoACento;
+        console.log('FizzBuzz');
     } else if (numeriDaUnoACento % 5 === 0 ) {
         console.log('Buzz');
-    } else if (numeriDaUnoACento % 3 === 0 && numeriDaUnoACento % 5 === 0 ) {
-        console.log('FizzBuzz');
+    } else if (numeriDaUnoACento % 3 === 0 ) {
+        console.log('Fizz');
+    } else {
+        console.log(numeriDaUnoACento);
     }
    
 }
